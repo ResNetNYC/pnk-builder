6ad03c918a31376778b0eadb6f17fc5aa61e2b34
Josh King <jking@chambana.net>
2019-01-08 20:51:02 -0500

    WIP

--- setup.sh
+++ setup.sh
@@ -1,50 +1,70 @@
 #!/bin/bash
 
-set -x
+set -eo pipefail
 
-PACKER_URL=https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
-PLUGIN_URL=https://github.com/solo-io/packer-builder-arm-image
-# Save pwd
-SRCDIR=`pwd`
+declare -r IMAGE_URL="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-11-15/2018-11-13-raspbian-stretch-lite.zip"
+declare -r IMAGE_SHA256SUM="47ef1b2501d0e5002675a50b6868074e693f78829822eef64f3878487953234d"
 
-# Pull Docker images
-docker pull arm64v8/mariadb:10
-docker pull arm64v8/wordpress:4
-docker pull ryansch/unifi-rpi:latest
+check_bin() {
+    local cmd = "$1"
+    hash "$cmd" 2>/dev/null || { printf "Need command %s but it is not found. Aborting." "$cmd"; exit 1; }
+}
 
-# Save Docker images
-mkdir import
-docker save -o import/mariadb.docker arm64v8/mariadb:10
-docker save -o import/wordpress.docker arm64v8/wordpress:4
-docker save -o import/unifi.docker ryansch/unifi-rpi:latest
+setup_chroot() {
+    local mnt="$1"
+    kpartx -a -v 
+}
 
-# Install Packer
-wget ${PACKER_URL} -O packer.zip
-unzip packer.zip packer
-# Check if plugin built and copy into place
-if [[ ! -f packer ]]; then {
-    echo "ERROR: Packer failed to install."
-    exit 1
-}; fi
+setup_salt() {
+}
 
+setup_docker() {
+}
 
-# Build plugin
-mkdir -p $GOPATH/src/github.com/solo-io/
-pushd $GOPATH/src/github.com/solo-io/
-# clean up potential residual files from previous builds
-rm -rf packer-builder-arm-image
-git clone --depth 1 ${PLUGIN_URL} packer-builder-arm-image
-pushd ./packer-builder-arm-image
-# Patch to force unmount
-patch -p1 < $SRCDIR/force-unmount.patch
-go build
+main() {
+    check_bin curl
+    check_bin docker
+    check_bin kpartx
+    check_bin mkdir
+    check_bin mount
+    check_bin rm
+    check_bin sha256sum
+    check_bin umount
 
-# Check if plugin built and copy into place
-if [[ ! -f packer-builder-arm-image ]]; then {
-    echo "ERROR: Plugin failed to build."
-    exit 1
-} else {
-    mkdir -p $HOME/.packer.d/plugins
-    cp packer-builder-arm-image $HOME/.packer.d/plugins/
-}; fi
+    # Create build directories
+    mkdir "$PWD/build"
+    mkdir "$PWD/cache"
+    local temp="$(mktemp -d)"
+    mkdir "$temp/mnt"
+
+    trap "{ umount -R -f "$temp"; rm -rf "$temp"; exit 0 }" EXIT
+    
+    # Pull Docker images
+    docker pull arm64v8/mariadb:10
+    docker pull arm64v8/wordpress:4
+    docker pull ryansch/unifi-rpi:latest
+    
+    if [[ ! -e "$PWD/cache/raspbian.zip" ]]; then
+        curl -o "$PWD/cache/raspbian.zip" -L "$IMAGE_URL"
+    fi
+    
+    if ! ( echo "$IMAGE_SHA256SUM $PWD/cache/raspbian.zip" | sha256sum -c ); then
+        echo "Invalid checksum for raspbian image. Aborting."
+        rm -f "$PWD/cache/raspbian.zip"
+        exit 1
+    else
+        unzip -d "$temp" "$PWD/cache/raspbian.zip"
+
+        
+
+    
+    # Bootstrap salt
+    curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
+    
+    # Save Docker images
+    mkdir import
+    docker save -o import/mariadb.docker arm64v8/mariadb:10
+    docker save -o import/wordpress.docker arm64v8/wordpress:4
+    docker save -o import/unifi.docker ryansch/unifi-rpi:latest
+}
 
