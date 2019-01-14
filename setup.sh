221d46063c39dcd45e18deee062c10a711b89b5a
Josh King <jking@chambana.net>
2019-01-13 22:37:44 -0500

    Additional debugging output.

--- setup.sh
+++ setup.sh
@@ -54,6 +54,7 @@ setup_chroot() {
         echo "Failed to map raspbian, invalid image?"
         return 1
     }
+    printf "Mounting %s and %s at %s." "${output[11]}" "${output[2]}" "$mount_dir"
 
     {
         mount "/dev/mapper/${output[11]}" "$mount_dir" && \
