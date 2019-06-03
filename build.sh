fd67294dc7a577b21d194bd3e6cd0234e750c9b0
Josh King <jking@chambana.net>
2019-06-03 13:44:45 -0400

    Update Raspbian image.

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -eo pipefail
 
 used_mktemp=false
 PNK_CONTAINERS=( "arm64v8/mariadb:10" "arm64v8/wordpress:4" "ryansch/unifi-rpi:latest" )
-: ${PNK_RPI_IMAGE_URL:="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2018-11-15/2018-11-13-raspbian-stretch-lite.zip"}
-: ${PNK_RPI_IMAGE_SHA256SUM:="47ef1b2501d0e5002675a50b6868074e693f78829822eef64f3878487953234d"}
+: ${PNK_RPI_IMAGE_URL:="https://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2019-04-09/2019-04-08-raspbian-stretch-lite.zip"}
+: ${PNK_RPI_IMAGE_SHA256SUM:="03ec326d45c6eb6cef848cf9a1d6c7315a9410b49a276a6b28e67a40b11fdfcf"}
 : ${PNK_SALT_SHA256SUM:="ab7f29b75711da4bb79aff98d46654f910d569ebe3e908753a3c5119017bb163"}
 : ${PNK_TEMP_DIR:="$(used_mktemp=true; mktemp -d)"}
 : ${PNK_CACHE_DIR:="$PNK_TEMP_DIR/cache"}
