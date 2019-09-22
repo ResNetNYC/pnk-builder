2da60dff39b55bef81ffe9738812770b375353fb
Josh King <jking@chambana.net>
2019-09-22 18:48:42 -0400

    Add services menu to wordpress, switch etherpad to mariadb and make docker networks more secure.

--- setup-wordpress.sh
+++ setup-wordpress.sh
@@ -7,15 +7,17 @@ export PNK_HOSTNAME="$(hostname -s)"
 
 check_bin() {
     local -r cmd="$1"
-    hash "$cmd" 2>/dev/null || { printf "Need command %s but it is not found. Aborting." "$cmd"; exit 1; }
+    hash "$cmd" 2>/dev/null || {
+        printf "Need command %s but it is not found. Aborting." "$cmd"
+        exit 1
+    }
 }
 
 main() {
     check_bin curl
     check_bin docker
 
-    until curl -sf -o /dev/null http://$PNK_HOSTNAME
-    do
+    until curl -sf -o /dev/null http://$PNK_HOSTNAME; do
         sleep 10
     done
 
@@ -23,7 +25,9 @@ main() {
         echo "Wordpress found but not installed. Installing."
         docker run --rm --volumes-from wordpress -v /srv/wordpress:/files -e PNK_HOSTNAME --network container:wordpress --user 33:33 arm32v7/wordpress:cli sh -c "\
             wp core install --path=/var/www/html --url=$PNK_HOSTNAME --title=PNK --admin_user=pnk --admin_password=pnk --admin_email=contact@pnkgo.com --skip-email;
-            wp plugin install --path=/var/www/html --activate /files/wordpress-importer.zip"
+            wp site empty --path=/var/www/html --yes;
+            wp plugin install --path=/var/www/html --activate /files/wordpress-importer.zip;
+            wp import --path=/var/www/html /files/pnk.WordPress.xml --authors=create"
         exit
     }
     echo "Wordpress already installed."
