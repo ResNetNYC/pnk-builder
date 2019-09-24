0fca5947fd3e8d22e2277cc91f35789f613d8428
Josh King <jking@chambana.net>
2019-09-24 10:36:11 -0400

    Fix a couple of typos in setup-wordpress.sh

--- post-install/setup-wordpress.sh
+++ post-install/setup-wordpress.sh
@@ -28,9 +28,9 @@ main() {
             wp site empty --path=/var/www/html --yes;
             wp plugin install --path=/var/www/html --activate /files/wordpress-importer.zip;
             wp import --path=/var/www/html /files/pnk.WordPress.xml --authors=create;
-            wp option update blogdescription "Portable Network Kit" --path=/var/www/html;
-            wp menu assign services menu-1 --path=/var/www/html;
-            wp menu assign services footer --path=/var/www/html"
+            wp option update blogdescription 'Portable Network Kit' --path=/var/www/html;
+            wp menu location assign services menu-1 --path=/var/www/html;
+            wp menu location assign services footer --path=/var/www/html"
         exit
     }
     echo "Wordpress already installed."
